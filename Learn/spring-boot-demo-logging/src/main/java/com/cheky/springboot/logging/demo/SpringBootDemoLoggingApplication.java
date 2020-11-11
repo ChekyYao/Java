a53509ea890e9c68a3b34e40f4fe9b6bc6fb83a6
Cheky Yao <13925568211@163.com>
2020-11-11 16:16:15 +0800

    Create a logging Template

---
+++ Learn/spring-boot-demo-logging/src/main/java/com/cheky/springboot/logging/demo/SpringBootDemoLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.cheky.springboot.logging.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoLoggingApplication.class, args);
+	}
+
+}
---
+++ Learn/spring-boot-demo-logging/src/test/java/com/cheky/springboot/logging/demo/SpringBootDemoLoggingApplicationTests.java
@@ -0,0 +1,18 @@
+package com.cheky.springboot.logging.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringBootDemoLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
