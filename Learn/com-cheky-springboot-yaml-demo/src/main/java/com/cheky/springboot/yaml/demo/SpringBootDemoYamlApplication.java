74946b7705a246909db887aff91347f48e733aa8
Cheky Yao <13925568211@163.com>
2020-11-12 08:53:22 +0800

    create a yaml empty template project

---
+++ Learn/com-cheky-springboot-yaml-demo/src/main/java/com/cheky/springboot/yaml/demo/SpringBootDemoYamlApplication.java
@@ -0,0 +1,13 @@
+package com.cheky.springboot.yaml.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoYamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoYamlApplication.class, args);
+	}
+
+}
---
+++ Learn/com-cheky-springboot-yaml-demo/src/test/java/com/cheky/springboot/yaml/demo/SpringBootDemoYamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheky.springboot.yaml.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoYamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
