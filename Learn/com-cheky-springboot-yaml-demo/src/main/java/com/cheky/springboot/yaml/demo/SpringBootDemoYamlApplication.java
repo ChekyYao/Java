4df0dce097bfa6c189e3c2c7e3e05d139cb5417e
Cheky Yao <13925568211@163.com>
2020-11-12 09:23:32 +0800

    yaml: 简单修改 server port https://www.bilibili.com/video/BV1Et411Y7tQ?p=9

--- Learn/com-cheky-springboot-yaml-demo/src/main/java/com/cheky/springboot/yaml/demo/SpringBootDemoYamlApplication.java
+++ Learn/com-cheky-springboot-yaml-demo/src/main/java/com/cheky/springboot/yaml/demo/SpringBootDemoYamlApplication.java
@@ -2,6 +2,9 @@ package com.cheky.springboot.yaml.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SpringBootDemoYamlApplication {
@@ -9,5 +12,4 @@ public class SpringBootDemoYamlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoYamlApplication.class, args);
 	}
-
 }
---
+++ Learn/com-cheky-springboot-yaml-demo/src/main/java/com/cheky/springboot/yaml/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.cheky.springboot.yaml.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/halo")
+    public String HelloWorld(){
+        return "Hello World";
+    }
+}
