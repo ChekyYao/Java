6a25e52d7077087f53ce9689f3a24427aafb793e
Cheky Yao <13925568211@163.com>
2020-11-12 10:13:22 +0800

    yaml: 配置Person 并在 测试获取 https://www.bilibili.com/video/BV1Et411Y7tQ?p=11

--- Learn/com-cheky-springboot-yaml-demo/src/test/java/com/cheky/springboot/yaml/demo/SpringBootDemoYamlApplicationTests.java
+++ Learn/com-cheky-springboot-yaml-demo/src/test/java/com/cheky/springboot/yaml/demo/SpringBootDemoYamlApplicationTests.java
@@ -1,13 +1,23 @@
 package com.cheky.springboot.yaml.demo;
 
+import com.cheky.springboot.yaml.demo.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringBootDemoYamlApplicationTests {
 
+	@Autowired
+	Person person;
+
 	@Test
 	void contextLoads() {
+		//test person which is in yaml
+		System.out.println(person);
 	}
 
 }
